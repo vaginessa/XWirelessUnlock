b65de81ed5bdc6e33c80444bf01f562e0d71b958
raidzero <paul.raidzero@gmail.com>
2014-05-19 19:51:40 -0600

    add signed APK & fix stats script

--- stats.sh
+++ stats.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-NUM_JAVA_FILES=`find . -name *.java | wc -l`
-NUM_JAVA_LINES=`find . -name *.java -exec cat {} \; | wc -l`
+NUM_JAVA_FILES=`find . -name "*.java" | wc -l`
+NUM_JAVA_LINES=`find . -name "*.java" -exec cat {} \; | wc -l`
 
-NUM_XML_FILES=`find . -name *.xml | wc -l`
-NUM_XML_LINES=`find . -name *.xml -exec cat {} \; | wc -l`
+NUM_XML_FILES=`find . -name "*.xml" | wc -l`
+NUM_XML_LINES=`find . -name "*.xml" -exec cat {} \; | wc -l`
 
 printf "$(basename `pwd`):\n$NUM_JAVA_LINES lines in $NUM_JAVA_FILES java files.\n$NUM_XML_LINES lines in $NUM_XML_FILES xml files.\n"
 
